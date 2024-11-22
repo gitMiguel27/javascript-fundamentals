498692e348855ff1af1424873f69193699bef58b
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2024-11-22 15:01:18 -0500

    deleted unnecessary code

--- index.js
+++ index.js
@@ -178,22 +178,3 @@ function getLearnerData(course, ag, submissions) {
 const results = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
 console.log(results);
-
-
-// THIS IS CODE THAT BELONGED INSIDE OF getLearnerData FUNCTION:
-    // const result = [
-    //     {
-    //     id: 125,
-    //     avg: 0.985, // (47 + 150) / (50 + 150)
-    //     1: 0.94, // 47 / 50
-    //     2: 1.0 // 150 / 150
-    //     },
-    //     {
-    //     id: 132,
-    //     avg: 0.82, // (39 + 125) / (50 + 150)
-    //     1: 0.78, // 39 / 50
-    //     2: 0.833 // late: (140 - 15) / 150
-    //     }
-    // ];
-
-    // return result;
